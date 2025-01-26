900a9e8ab793bec96259657800fb80da069cbc02
BryOk-droid <brianokoth534@gmail.com>
2025-01-26 17:16:21 +0300

    Define Akan names mapping for males and females

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+const AKAN_NAMES = {
+  male: {
+    0: "Kwasi", // Sunday
+    1: "Kwadwo", // Monday
+    2: "Kwabena", // Tuesday
+    3: "Kwaku", // Wednesday
+    4: "Yaw", // Thursday
+    5: "Kofi", // Friday
+    6: "Kwame", // Saturday
+  },
+  female: {
+    0: "Akosua", // Sunday
+    1: "Adwoa", // Monday
+    2: "Abenaa", // Tuesday
+    3: "Akua", // Wednesday
+    4: "Yaa", // Thursday
+    5: "Afua", // Friday
+    6: "Ama", // Saturday
+  },
+};
